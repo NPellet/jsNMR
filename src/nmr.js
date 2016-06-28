35c35934a922dd6d25bf973245ced737b2bf4013
NPellet <norman.pellet@gmail.com>
2016-06-28 16:03:25 +0200

    Fix scaling + new jsGraph version

--- src/nmr.js
+++ src/nmr.js
@@ -1,4 +1,4 @@
-define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter' ], function( $, Graph, Shape1DNMR, Assignment, JcampConverter ) {
+define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter', '../lib/components/eventEmitter/EventEmitter' ], function( $, Graph, Shape1DNMR, Assignment, JcampConverter, EE ) {
 
 	// Root here
 	var defaults = {
@@ -90,6 +90,8 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 		shape.draw();
 		shape.redraw();
 
+
+
 		return shape;
 	}
 	
@@ -152,12 +154,17 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 			this.assignement = new Assignment( $.extend( this.options.assignment, { graphs: this.graphs, domGraphs: this.options.dom } ) );	
 			//this.assignement.setAssignment( [ [ "1", "gGQHLIeIUjdA~dPHeT" ] ] );
 		}
+
+
+		this.heightIntegral1 = 150;
 	};
 
 
 	var loadDefaults = {
 		urls: {}
 	};
+
+	NMR.prototype = new EE();
 
 	NMR.prototype.load = function( load ) {
 
@@ -190,18 +197,24 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 		}
 	
 		this.integrals.push( integral );
+
+		this.emit( "integralCreated" );
 		//integrals.push( integral );
 		//nmrint.originalShape = integral;
 	}
 
 	NMR.prototype.integralChanged = function( integral ) {
 		this.recalculateIntegrals( );
+
+		this.emit( "integralChanged" );
 	}
 
 	NMR.prototype.integralRemoved = function( integral ) {
 
 		nmr.integrals.splice( nmr.integrals.indexOf( i ), 1 );
 		nmr.recalculateIntegrals( );
+
+		this.emit( "integralRemoved" );
 	}
 
 
@@ -211,9 +224,10 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 			numberOfIntegrals = nmr.integrals.length;
 
 		if( numberOfIntegrals == 1 ) {
-			nmr.ratio = 150 / nmr.integrals[ 0 ].sum;
+			nmr.ratio = nmr.heightIntegral1 / nmr.integrals[ 0 ].sum;
 			nmr.ratioSum = nmr.integrals[ 0 ].sum;
 		}
+
 		
 		for( var i = 0; i < numberOfIntegrals ; i ++ ) {
 
@@ -229,45 +243,34 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 		}
 	}
 
-	NMR.prototype.integralValueChanged = function( integral ) {
+	NMR.prototype.redrawIntegrals = function() {
 
-		var fl = parseFloat( integral.getLabelText( 0 ) );
-		ratioSum = integral.sum / fl;
-		this.recalculateIntegrals( );
+		var l = this.integrals.length;
+		for( var i = 0; i < l; i++ ) {
+			this.integrals[ i ].redraw();
+		}
 	}
 
 
-	NMR.prototype.isSymmetric = function() {
-		return this.options.symmetric || false;
-	};
+	NMR.prototype.integralValueChanged = function( integral ) {
 
-	NMR.prototype.getMode = function() {
-		return this.options.mode;
-	};
+		var fl = parseFloat( integral.getLabelText( 0 ) );
+		this.ratioSum = integral.sum / fl;
+		this.recalculateIntegrals( );
+		this.emit( "integralValueChanged" );
+	}
+
 
 	NMR.prototype.getDom = function() {
 		return this.options.dom;
 	};
 
-	NMR.prototype.getGraph2D = function() {
-		return this.graphs['_2d'];
+	NMR.prototype.getGraph = function() {
+		return this.graphs;
 	};
 
-	NMR.prototype.getGraphX = function() {
-		return this.graphs['x'];
-	};
-
-	NMR.prototype.getGraphY = function() {
-		return this.graphs['y'];
-	};
-
-	NMR.prototype.resize1DTo = function( w, h ) {
+	NMR.prototype.resizeTo = function( w, h ) {
 		this.graphs.resize( w, h );
-		this.graphs.drawSeries();
-	};
-
-	NMR.prototype.removeSerieX = function( name ) {
-		removeSerie( this, 'x', name );
 	};
 
 	NMR.prototype.setSerieX = function( name, data, options ) {
@@ -313,18 +316,87 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 
 
 	NMR.prototype.loaded = function( series, options, name ) {
-		
-		switch( this.getMode() ) {
+	
+define( function() {
 
-			case '1d':
+	return [ function( domGraph ) {
 
-				this.setSerieX( name, series.x.spectra[ 0 ].data[ 0 ], { label: "SomeLabel" } );
-
-
-			break;
+	var graph = new Graph( domGraph, { 
 			
+		plugins: {
+			 'zoom': { zoomMode: 'xy', transition: true },
+			 'drag': {
+		          persistanceX: true,
+		          dragY: false
+		        },
+
 		}
+	}, { });
+
+	var data = [];
+	var colors = [];
+//	http://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+	/* accepts parameters
+	 * h  Object = {h:x, s:y, v:z}
+	 * OR 
+	 * h, s, v
+	*/
+
+	var hexChar = ["0", "1", "2", "3", "4", "5", "6", "7","8", "9", "A", "B", "C", "D", "E", "F"];
+
+	function byteToHex(b) {
+	  return hexChar[(b >> 4) & 0x0f] + hexChar[b & 0x0f];
+	}
+
+	function HSVtoRGB(h, s, v) {
+	    var r, g, b, i, f, p, q, t;
+	    if (arguments.length === 1) {
+	        s = h.s, v = h.v, h = h.h;
+	    }
+	    i = Math.floor(h * 6);
+	    f = h * 6 - i;
+	    p = v * (1 - s);
+	    q = v * (1 - f * s);
+	    t = v * (1 - (1 - f) * s);
+	    switch (i % 6) {
+	        case 0: r = v, g = t, b = p; break;
+	        case 1: r = q, g = v, b = p; break;
+	        case 2: r = p, g = v, b = t; break;
+	        case 3: r = p, g = q, b = v; break;
+	        case 4: r = t, g = p, b = v; break;
+	        case 5: r = v, g = p, b = q; break;
+	    }
+	    return "#" + byteToHex( Math.floor(r * 255) ) + byteToHex( Math.floor(g * 255) ) + byteToHex( Math.floor(b * 255) );
+	}
+
+	for( var i = 0; i < Math.PI * 10; i += 0.001 ) {
+		data.push( i );
+		data.push( Math.sin( i ) );
+		colors.push( HSVtoRGB( Math.pow(Math.sin( i ), 2), 0.8, 0.8 ) );
+	}
+
 		
+		var s = graph.newSerie("a", {}, "line.color").autoAxis().setData(data);
+		s.setColors([ colors ]);
+		graph.draw();
+		var date = Date.now();
+		graph.drawSeries( true );
+
+var j = 0;
+		for( var i in s.lines ) {
+			j++;
+		}
+		console.log( j );
+
+	}, 
+
+		"Basic example", 
+		[ 'Setting up a chart takes only a couple lines. Call <code>new Graph( domElement );</code> to start a graph. Render it with <code>graph.redraw();</code>', 'To add a serie, call <code>graph.newSerie( "serieName" )</code>. To set data, call <code>serie.setData()</code> method.'] 
+	];
+
+
+} );
+	this.setSerieX( name, series.x.spectra[ 0 ].data[ 0 ], { label: "SomeLabel" } );
 	};
 
 
@@ -381,29 +453,23 @@ define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter'
 			},
 
 
-			dblclick: {
-				type: 'plugin',
-				plugin: 'zoom',
-				options: {
-					mode: 'total'
-				}
-			},
+			mouseActions: [
+				{ plugin: 'zoom', shift: false, ctrl: false },
+				{ plugin: 'shape', shift: true, ctrl: false },
+				{ plugin: 'zoom', shift: false, type: 'mousewheel', options: { baseline: 0, direction: 'y' } },
+				{ shift: true, type: 'mousewheel', callback: function( delta ) {
+
+					var change = delta / 300;
+					self.heightIntegral1 += change;
+					self.ratio = self.heightIntegral1 / self.integrals[ 0 ].sum;
+					self.recalculateIntegrals();
+					self.redrawIntegrals();
+				} },
+				{ plugin: 'zoom', shift: false, type: 'dblclick', options: { mode: 'gradualXY' } }
+
+			],
 
 
-			wheel: {
-				type: 'plugin',
-				plugin: 'zoom',
-				options: {
-					direction: 'y',
-					baseline: 0
-				}
-			},
-
-
-			pluginAction: {
-				'zoom': { shift: false, ctrl: false },
-				'shape': { shift: true, ctrl: false }
-			},
 
 			onBeforeNewShape: function() {
 
