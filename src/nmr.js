ee62e31cfad6ffe47d6531b659f4a165fbb7bcc0
Michaël Zasso <targos@protonmail.com>
2016-06-29 08:23:59 +0200

    give a name to nmr module

--- src/nmr.js
+++ src/nmr.js
@@ -1,4 +1,4 @@
-define( [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter', 'eventEmitter' ], function( $, Graph, Shape1DNMR, Assignment, JcampConverter, EE ) {
+define( 'nmr', [ 'jquery', 'jsgraph', './shape.1dnmr', './assignment', 'jcampconverter', 'eventEmitter' ], function( $, Graph, Shape1DNMR, Assignment, JcampConverter, EE ) {
 
 	// Root here
 	var defaults = {
